f35ad2cbcd4358c4320fc99431866789c5c3f119
ashok34kumar <163623192+ashok34kumar@users.noreply.github.com>
2024-08-07 14:34:56 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,12 @@
+function multiplicationBy() {
+const    num1 = document.getElementById("firstNumber").value;
+ const   num2 = document.getElementById("secondNumber").value;
+
+    document.getElementById('result').innerHTML = (num1 * num2);
+    }
+function divisionBy() {
+const    num1 = document.getElementById('firstNumber').value;
+const    num2 = document.getElementById('secondNumber').value;
+
+    document.getElementById('result').innerHTML = num1 / num2;
+    }
